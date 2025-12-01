80582cb7f6de1886ae6d3c18ee850dd9bde27c8a
jkhiba2023 <khan.jabir1055@gmail.com>
2025-12-02 00:31:48 +0530

    Total sum of cart product

--- src/Components/Card/CartList.jsx
+++ src/Components/Card/CartList.jsx
@@ -6,17 +6,25 @@ const CartList = () => {
 
   console.log("cartItems", cartItems);
 
+  const totalPrice = cartItems.reduce((accumulatore, currentItem) => {
+    return accumulatore + currentItem.price;
+  }, 0);
+
   return (
     <ul>
       {cartItems.map((item) => {
         return (
-          <li className="flex items-center gap-2 border-1">
+          <li className="flex items-center gap-2">
             <img src={item.images[0]} alt="" className="w-20" />
-            <p className="">{item.title}</p>
+            <p className="flex items-center">{item.title}</p>
             <p className="ml-auto mr-5">${item.price}</p>
           </li>
         );
       })}
+      <div className="flex ">
+        <p>Total</p>
+        <p>{totalPrice}</p>
+      </div>
     </ul>
   );
 };
