83ac44c1d82dc4ab14461910c7bdb32fd2d68ec9
jkhiba2023 <khan.jabir1055@gmail.com>
2026-01-23 20:25:29 +0530

    Adding all categories items in Home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,30 @@ import img1 from "../../assets/Home1.webp";
 import { GiReturnArrow } from "react-icons/gi";
 import { IoMdCash } from "react-icons/io";
 import { MdAutoAwesome, MdHighQuality } from "react-icons/md";
-import { ImgData } from "../ImageData/imageData";
+import Beauty from "../../assets/Beauty.png";
+import Fragrances from "../../assets/Fragrances.png";
+import Furniture from "../../assets/Furniture.png";
+import Groceries from "../../assets/Groceries.png";
+import HomeDecoration from "../../assets/HomeDecoration.png";
+import KitchenAccessories from "../../assets/KitchenAccessories.png";
+import Laptops from "../../assets/Laptops.png";
+import MensShirts from "../../assets/MensShirts.png";
+import MensShoes from "../../assets/MensShoes.png";
+import MensWatches from "../../assets/MensWatches.png";
+import MobileAccessories from "../../assets/MobileAccessories.png";
+import Motorcycle from "../../assets/Motorcycle.png";
+import SkinCare from "../../assets/SkinCare.png";
+import Smartphones from "../../assets/Smartphones.png";
+import SportsAccessories from "../../assets/SportsAccessories.png";
+import Sunglasses from "../../assets/Sunglasses.png";
+import Tablets from "../../assets/Tablets.png";
+import Tops from "../../assets/Tops.png";
+import Vehicle from "../../assets/Vehicle.png";
+import WomensBags from "../../assets/WomensBags.png";
+import WomensDresses from "../../assets/WomensDresses.png";
+import WomensJewellery from "../../assets/WomensJewellery.png";
+import WomensShoes from "../../assets/WomensShoes.png";
+import WomensWatches from "../../assets/WomensWatches.png";
 import { Link } from "react-router-dom";
 
 const Home = () => {
@@ -18,6 +41,33 @@ const Home = () => {
       })
       .catch((err) => console.error("Error fetching:", err));
   }, []);
+
+  const ImgData = {
+    Beauty,
+    Fragrances,
+    Furniture,
+    Groceries,
+    "Home Decoration": HomeDecoration,
+    "Kitchen Accessories": KitchenAccessories,
+    Laptops,
+    "Mens Shirts": MensShirts,
+    "Mens Shoes": MensShoes,
+    "Mens Watches": MensWatches,
+    "Mobile Accessories": MobileAccessories,
+    Motorcycle,
+    "Skin Care": SkinCare,
+    Smartphones,
+    "Sports Accessories": SportsAccessories,
+    Sunglasses,
+    Tablets,
+    Tops,
+    Vehicle,
+    "Womens Bags": WomensBags,
+    "Womens Dresses": WomensDresses,
+    "Womens Jewellery": WomensJewellery,
+    "Womens Shoes": WomensShoes,
+    "Womens Watches": WomensWatches,
+  };
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen w-full bg-gray-100 overflow-hidden">
